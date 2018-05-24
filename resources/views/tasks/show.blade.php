31fd618768eec3d914bb092240427a1930327af5
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-05-24 03:24:00 +0000

    add status

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,9 +4,10 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
-    <p>{{ $task->content }}</p>
+    <p>ステータス: {{ $task->status }}</p>
+    <p>タスク: {{ $task->content }}</p>
     
-    {!! link_to_route('tasks.edit', 'タスクを編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
